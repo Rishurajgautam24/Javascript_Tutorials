226a99ec7b5fb35aa4395552fbeef7a9836db016
RISHU RAJ GAUTAM <Rishu24oct@gmail.com>
2024-05-10 12:32:49 +0530

    Tutorial 1 files added

---
+++ Chapter 01/Tutorial 02.js
@@ -0,0 +1,20 @@
+// Intro to Variables
+
+// Variables can store information
+// we can use that information later 
+// we can change that information later as well 
+
+// Declare a varibale
+var firstName = "Hello Rishh";
+
+// Use a variable
+console.log(firstName);
+
+// Changing the value of variable
+// We don't need to write var again var is used to just declare variable
+firstName = "Mohit";
+console.log(firstName);
+
+
+
+// "use strict"; is used if we don't want to create variable without var tag
---
+++ Chapter 01/Tutorial 03.js
@@ -0,0 +1,26 @@
+// Rules for naming of variables 
+
+// You cannot start with numbers
+// Example 
+// 1name ( Invalid )
+// name1 ( Valid )
+
+// var 1name = 10;
+var name1 = 10;
+
+// console.log(1name);
+console.log(name1);
+
+
+// You can use only uderscore(_) or dollor
+// first_name (Valid)
+// _firstname (Valid)
+
+// first$name (Valid)
+// $firstname (Valid)
+
+//  You cannot use Spaces
+// first name ( Invalid)
+
+// Convention
+// Use camelCase i.e., start with small letter and 2nd word's first letter as Capital
